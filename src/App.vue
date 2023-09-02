bea236ad0d72fe42193945ffcf65b251e329e9b4
Doffy_Fastcodingvn <doffy.fastcodingvn@gmail.com>
2023-09-02 16:40:19 +0700

    [fix] css to sass

--- src/App.vue
+++ src/App.vue
@@ -17,31 +17,25 @@ import TheWelcome from './components/TheWelcome.vue'
   </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<style scoped lang="sass">
+header
+  line-height: 1.5
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+.logo
+  display: block
+  margin: 0 auto 2rem
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+@media (min-width: 1024px)
+  header
+    display: flex
+    place-items: center
+    padding-right: calc(var(--section-gap) / 2)
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+  .logo
+    margin: 0 2rem 0 0
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
+  header .wrapper
+    display: flex
+    place-items: flex-start
+    flex-wrap: wrap
 </style>
