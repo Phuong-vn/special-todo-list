5b12c06264696f14f3eabdf825baf8499406b7e5
Doffy_Fastcodingvn <doffy.fastcodingvn@gmail.com>
2023-09-03 20:02:37 +0700

    [upd] component

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+<template>
+  <header class="header">
+    <h1>todo</h1>
+    <input class="new-todo" placeholder="What needs to be done?" autofocus>
+  </header>
+</template>
